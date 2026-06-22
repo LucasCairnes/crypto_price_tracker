d09e1a3eb9413f6564f50775a2978371d487f6fc
Lucas <lucas@evidentinsights.com>
2026-06-22 22:52:01 +0100

    Write SQL for hypertable

---
+++ init.sql
@@ -0,0 +1,13 @@
+CREATE EXTENSION IF NOT EXISTS timescaledb;
+
+CREATE TABLE IF NOT EXISTS enriched_trades (
+    symbol        TEXT             NOT NULL,
+    window_start  TIMESTAMPTZ      NOT NULL,
+    window_end    TIMESTAMPTZ      NOT NULL,
+    vwap          DOUBLE PRECISION NOT NULL,
+    volume        DOUBLE PRECISION NOT NULL,
+    trade_count   BIGINT           NOT NULL,
+    PRIMARY KEY (symbol, window_start)
+);
+
+SELECT create_hypertable('enriched_trades', 'window_start', if_not_exists => TRUE);
